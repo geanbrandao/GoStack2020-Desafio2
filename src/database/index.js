37df5d619f43ecf7d532f6f77bb81da693bcfda5
Gean Brandao <gean.kar@brave.ag>
2020-02-11 14:51:26 -0200

    create get, remove, update e put com verificação se o admin é o dono do cadastro do destinatario

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import Admin from '../app/models/AdminUsers';
+import Recipients from '../app/models/Recipients';
 
 import databaseConfig from '../config/database';
 
-const models = [Admin];
+const models = [Admin, Recipients];
 
 class Database {
   constructor() {
